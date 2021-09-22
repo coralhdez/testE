698e5be4a086205e7d2b0c19d77b7e3488c10a22
coralhdez <coralhernandezg@gmail.com>
2021-09-22 12:00:21 +0200

    cambios

---
+++ proyectoE/src/proyectoe/ProyectoE.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoe;
+
+/**
+ *
+ * @author Alumno Mañana
+ */
+public class ProyectoE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      var alumno = "Lucia";
+      var titulo = "informatico";
+       
+      var concat = titulo + " " + alumno;
+      
+        System.out.println("concat = " + concat );
+      
+      
+    }
+    
+}
